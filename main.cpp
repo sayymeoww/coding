7afa6439b5fa8a53c21235fca48520ea135eda07
saytreep <treep.me@yandex.ru>
2023-07-02 17:34:03 +0300

    idk 4

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include    <limits>
 #include    <numbers>
 #include    <cassert>
- 
+
 #include    <map>
 #include    <set>
 #include    <queue>
@@ -11,25 +11,25 @@
 #include    <vector>
 #include    <string>
 #include    <cstring>
- 
+
 #include    <cmath>
 #include    <bitset>
 #include    <algorithm>
- 
+
 //other includes here
- 
+
 #define     int     long long
 #define     pi      numbers::pi //type : double
 #define     inf     numeric_limits<int>::max()
- 
+
 using namespace std;
 
 int32_t main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
- 
+
     //start coding here >.<
- 
+
     return 0;
 }
