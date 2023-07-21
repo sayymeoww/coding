a17dec88f15fd53365c06589886267f331bd9bc8
saytreep <treep.me@yandex.ru>
2023-07-21 16:07:04 +0300

    edit 4 and 5 solutions

--- tasks (demian)/day 6/task 4/task 4.cpp
+++ tasks (demian)/day 6/task 4/task 4.cpp
@@ -1,60 +1,15 @@
-#include    <iostream>
-#include    <iomanip>
-#include    <cstdint>
-#include    <fstream>
-#include    <limits>
-#include    <numbers>
-#include    <cassert>
+int n;
+cin >> n;
 
-#include    <map>
-#include    <set>
-#include    <queue>
-#include    <deque>
-#include    <vector>
-#include    <string>
-#include    <cstring>
-
-#include    <cmath>
-#include    <bitset>
-#include    <algorithm>
-
-// ? other includes here
-
-#define     int     long long
-#define     pi      numbers::pi //type : double
-#define     inf     numeric_limits<int>::max()
-
-// ? other defines here
-
-using namespace std;
-
-// mind -> maxd = n / mind
-// maxd x (n / maxd)
-// r = (1 / 2) * d = ceil(1 / 2 * sqrt(maxd^2 + (n / maxd)^2))
-
-int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    // * start coding here
-    // * (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
-
-    int n;
-    cin >> n;
-
-    int d = 1;
-    for (int i = sqrt(n); i >= 2; i++) {
-        if (n % i == 0) {
-            d = i;
-            break;
-        }
+int d = 1;
+for (int i = sqrt(n); i >= 2; i++) {
+    if (n % i == 0) {
+        d = i;
+        break;
     }
-
-    int a = d;
-    int b = n / d;
-
-    cout << ceil(0.5 * sqrt(a * a + b * b)) << '\n';
-
-    return 0;
 }
+
+int a = d;
+int b = n / d;
+
+cout << ceil(0.5 * sqrt(a * a + b * b)) << '\n';
--- tasks (demian)/day 6/task 5/task 5.cpp
+++ tasks (demian)/day 6/task 5/task 5.cpp
@@ -1,53 +1,12 @@
-#include    <iostream>
-#include    <iomanip>
-#include    <cstdint>
-#include    <fstream>
-#include    <limits>
-#include    <numbers>
-#include    <cassert>
+int t;
+cin >> t;
 
-#include    <map>
-#include    <set>
-#include    <queue>
-#include    <deque>
-#include    <vector>
-#include    <string>
-#include    <cstring>
+int n, a, b, c;
+cin >> n >> a >> b >> c;
 
-#include    <cmath>
-#include    <bitset>
-#include    <algorithm>
-
-// ? other includes here
-
-#define     int     long long
-#define     pi      numbers::pi //type : double
-#define     inf     numeric_limits<int>::max()
-
-// ? other defines here
-
-using namespace std;
-
-int32_t main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-
-    // * start coding here
-    // * (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
-
-    int t;
-    cin >> t;
-
-    int n, a, b, c;
-    cin >> n >> a >> b >> c;
-
-    if (t == 2) {
-        cout << min({a, b, c}) << '\n';
-    } else {
-        int answer = a + b + c - 2 * n;
-        cout << ((answer < 0) ? 0 : answer) << '\n';
-    }
-
-    return 0;
+if (t == 2)
+    cout << min({a, b, c}) << '\n';
+else {
+    int answer = a + b + c - 2 * n;
+    cout << ((answer < 0) ? 0 : answer) << '\n';
 }
