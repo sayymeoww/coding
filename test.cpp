7da3d7aa8590f22c5af13287fa6920ab7663966f
saytreep <treep.me@yandex.ru>
2023-09-11 18:23:54 +0300

    add test.cpp

---
+++ test.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <cmath>
+#include <cstdint>
+#include <cstring>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <map>
+#include <numbers>
+#include <queue>
+#include <set>
+#include <string>
+#include <vector>
+
+// ? other includes here
+
+#define int long long
+#define pi numbers::pi  // type : double
+#define inf numeric_limits<int>::max()
+
+#define debug(x) cout << (#x) << " -> " << x << '\n';
+
+// ? other defines here
+
+using namespace std;
+
+class init {
+    public:
+        init() { cout << fixed << setprecision(10); }
+
+        ~init() {
+            cerr << "⌛ time: " << static_cast<double>(clock()) / CLOCKS_PER_SEC
+                 << " s\n";
+        }
+} init;
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // * start coding here
+    // * (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
+
+    return 0;
+}
