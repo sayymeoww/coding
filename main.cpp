8137177c16587d4fe8dcb39aa3e08598810b892e
saytreep <treep.me@yandex.ru>
2023-10-09 13:12:02 +0300

    changed template

--- main.cpp
+++ main.cpp
@@ -20,6 +20,7 @@
 // ? other includes here
 
 #define int long long
+#define ll uint64_t
 #define pi numbers::pi  // type : double
 #define inf numeric_limits<int>::max()
 
@@ -29,13 +30,20 @@
 
 using namespace std;
 
+// ! declared program time
+const double TIME = 1;  // in seconds
+
 class init {
     public:
         init() { cout << fixed << setprecision(10); }
 
         ~init() {
-            cerr << "⌛ time: " << static_cast<double>(clock()) / CLOCKS_PER_SEC
-                 << " s\n";
+            double PROGTIME = static_cast<double>(clock()) / CLOCKS_PER_SEC;
+            cerr << "⌛ time: " << PROGTIME << " s ";
+            if (PROGTIME > TIME)
+                cerr << "[❌ TL ERROR]" << '\n';
+            else
+                cerr << "[✅ OK]" << '\n';
         }
 } init;
 
