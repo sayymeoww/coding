cebbfb634b61e0ab3a964c1686479b377fe98949
saytreep <treep.me@yandex.ru>
2023-11-09 17:34:26 +0300

    another another commit

--- main.cpp
+++ main.cpp
@@ -53,9 +53,10 @@
 #pragma GCC optimize("-fexpensive-optimizations")
 #pragma GCC optimize("inline-functions-called-once")
 #pragma GCC optimize("-fdelete-null-pointer-checks")
-#pragma endregion pragmas
 
 // ? other pragmas
+
+#pragma endregion pragmas
 
 #pragma region includes
 #include <algorithm>
@@ -80,9 +81,10 @@
 #include <stack>
 #include <string>
 #include <vector>
-#pragma endregion includes
 
 // ? other includes here
+
+#pragma endregion includes
 
 #pragma region defines
 #define int long long
@@ -97,11 +99,12 @@
 
 #define all(x) begin(x), end(x)
 #define print(x) \
-    for (auto &el : x) cout << el + 1 << " "
+    for (auto &el : x) cout << el << " "
 #define sort(x) sort(x.begin(), x.end())
-#pragma endregion defines
 
 // ? other defines here
+
+#pragma endregion defines
 
 using namespace std;
 
@@ -143,68 +146,6 @@ signed main() {
 
     // * start coding here
     // * (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
-
-    int n, m, k, pos;
-    cin >> n >> m;
-
-    char c;
-    string s;
-    cin >> s;
-
-    vector<vector<pair<int, char>>> chars(n), poses(m);
-    vector<int> bad(n);
-
-    for (int i = 0; i < n; i++) {
-        cin >> k;
-        for (int j = 0; j < k; j++) {
-            cin >> pos >> c;
-            pos--;
-
-            chars[i].push_back({pos, c});
-            poses[pos].push_back({i, c});
-
-            if (c != s[pos]) bad[i]++;
-        }
-    }
-
-    vector<bool> perf(n, false), poss(m, false);
-
-    vector<int> p;
-    set<pair<int, int>> q;
-
-    for (int i = 0; i < n; i++) q.insert({bad[i], i});
-
-    while (!q.empty()) {
-        auto it = q.begin();
-        int card = it->second;
-
-        if (bad[card] != 0) break;
-
-        q.erase(it);
-        p.push_back(card);
-
-        perf[card] = true;
-
-        for (auto [pos, c] : chars[card])
-            if (!poss[pos]) {
-                poss[pos] = true;
-                for (auto [badd, c2] : poses[pos]) {
-                    if (!perf[badd] && c2 != c) {
-                        q.erase({bad[badd], badd});
-                        bad[badd]--;
-                        q.insert({bad[badd], badd});
-                    }
-                }
-            }
-    }
-
-    bool flag = true;
-    for (bool have : poss) flag &= have;
-
-    if (p.size() != n || !flag)
-        cout << -1 << '\n';
-    else
-        print(p);
 
     return 0;
 }
