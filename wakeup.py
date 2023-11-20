69c5ec604ee58f694631bbd21b4aff5c4370041f
saytreep <treep.me@yandex.ru>
2023-11-20 17:47:40 +0300

    another commit

--- wakeup.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-subprocess.run('alias run="python3 TEST/test.py"', shell=True)
-subprocess.run('alias end="python3 TEST/clear.py"', shell=True)
-subprocess.run('alias setup="python3 SETUP/setup.py"', shell=True)
-subprocess.run('alias generate="python3 TEST/generate.py"', shell=True)
---
+++ wakeup.sh
@@ -0,0 +1,4 @@
+alias run="python3 TEST/test.py"
+alias end="python3 TEST/clear.py"
+alias setup="python3 SETUP/setup.py"
+alias generate="python3 TEST/generate.py"
