2d1a544c049c7269e9751fc9f99157b64741d291
saytreep <treep.me@yandex.ru>
2023-12-17 22:02:14 +0300

    added cses folder

---
+++ cses/template.cpp
@@ -0,0 +1,77 @@
+#pragma region template
+#pragma region includes
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+
+// ? other includes
+
+#pragma endregion includes
+
+#pragma region defines
+#define int long long
+#define ll uint64_t
+#define pi numbers::pi  // type : double
+#define inf numeric_limits<int>::max()
+
+#define debug(x) cout << (#x) << " -> " << x << '\n';
+
+#define bin(x) 1 << x
+#define log(x) 32 - __builtin_clz(x) - 1
+
+#define all(x) begin(x), end(x)
+#define print(x) \
+    for (auto &el : x) cout << el << " "
+#define sort(x) sort(x.begin(), x.end())
+
+// ? other defines
+
+#pragma endregion defines
+
+const int mod = 1e9 + 7;
+const int TIME = 1;  // in seconds
+using namespace std;
+
+#pragma region init
+class init {
+   public:
+    init() { cout << fixed << setprecision(10); }
+
+    ~init() {
+        double PROGTIME = static_cast<double>(clock()) / CLOCKS_PER_SEC;
+        cerr << fixed << setprecision(6) << "⌛ time : " << PROGTIME << " s \n";
+    }
+} init;
+#pragma endregion init
+#pragma endregion template
+
+// * [˙·٠•●♥ Solution ♥●•٠·˙] * //
+
+signed main() {
+#pragma region ios
+    cin.tie(nullptr)->sync_with_stdio(false);
+    cout.tie(nullptr);
+    cout.precision(20);
+
+    // fstream cin("file/input.txt");
+    // ofstream cout("file/output.txt");
+#pragma endregion ios
+
+    // * start coding here
+    // * (づ°ω°)づﾐe★゜・。。・゜゜・。。・゜☆゜・。。・゜゜・。。・゜
+
+    cout.flush();
+    return 0;
+}
